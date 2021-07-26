72845fe22e7a8eb7bcc526100769234ca835cf83
wasswa <williamwasswa036@gmail.com>
2021-07-26 20:05:07 +0300

    Add files via upload

---
+++ public/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+from django.db import models
+admin.site.register( Service )
+admin.site.register( ServiceProvider )
+admin.site.register( Status )
+admin.site.register( Classe )
+admin.site.register( Subject )
+admin.site.register( LockdownPackage )
